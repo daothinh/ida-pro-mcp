b7bce3885870d8d565982decea47806d24fd8ebb
daothinh <daocuongthinh223@gmail.com>
2025-05-07 11:39:46 +0700

    custom host

--- src/ida_pro_mcp/server.py
+++ src/ida_pro_mcp/server.py
@@ -14,6 +14,7 @@ mcp = FastMCP("github.com/mrexodia/ida-pro-mcp", log_level="ERROR")
 
 jsonrpc_request_id = 1
 ida_host = "127.0.0.1"
+# ida_host = "192.168.124.244"
 ida_port = 13337
 
 def make_jsonrpc_request(method: str, *params):
